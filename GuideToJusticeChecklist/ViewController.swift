0d423c2ec835625b05cc18fa02885b1623c001af
Nick Dareshori <ndareshori@elon.edu>
2020-07-15 19:04:26 -0400

    Initial Commit

---
+++ GuideToJusticeChecklist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuideToJusticeChecklist
+//
+//  Created by Nick Dareshori on 7/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
